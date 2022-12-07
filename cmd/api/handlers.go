17bad2a43fda98039d37a95f048e0fa572c42390
Luca Sciarra <sciarra.l.m@gmail.com>
2022-12-06 20:39:14 -0500

    update actions to create and auth user

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -11,13 +13,23 @@ import (
 )
 
 type RequestPayload struct {
-	Action string      `json:"action"`
-	Auth   AuthPayload `json:"auth,omitempty"`
+	Action      string            `json:"action"`
+	Auth        AuthPayload       `json:"auth,omitempty"`
+	CreateUser  CreateUserPayload `json:"createuser,omitempty"`
+	PasetoToken string            `json:"paseto,omitempty"`
 }
 
 type AuthPayload struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type CreateUserPayload struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
 }
 
 func (app *Config) ExecuteRequest(w http.ResponseWriter, r *http.Request) {
@@ -31,50 +43,100 @@ func (app *Config) ExecuteRequest(w http.ResponseWriter, r *http.Request) {
 
 	// switch on different actions requested
 	switch requestPayload.Action {
+	case "createuser":
+		app.create_user(w, requestPayload.CreateUser)
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+
+	default:
+		app.errorJSON(w, fmt.Errorf("error. invalid option"))
 	}
+
 }
 
-// a function that utilizes a GRPC conneciton to the auth
-// service to validify an email and a password
-func (app *Config) authenticate(w http.ResponseWriter, authPayload AuthPayload) {
-
-	conn, err := grpc.Dial("ditto:50001", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+func (app *Config) create_user(w http.ResponseWriter, cruPayload CreateUserPayload) {
+	log.Println("Done...")
+	conn, err := grpc.Dial("localhost:50001", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
 		app.errorJSON(w, err)
 		return
 	}
-
 	defer conn.Close()
 
 	a := auth.NewAuthServiceClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-
-	authResp, err := a.AuthUser(ctx, &auth.AuthRequest{
-		ArgUser: &auth.User{
-			Username: authPayload.Username,
-			Password: authPayload.Password,
+	log.Println("Done...")
+	cruResp, err := a.CreateUser(ctx, &auth.CreateUserRequest{
+		ArgUser: &auth.M_User{
+			Email:     cruPayload.Email,
+			Username:  cruPayload.Username,
+			FirstName: cruPayload.FirstName,
+			LastName:  cruPayload.LastName,
 		},
+		Password: cruPayload.Password,
 	})
+
+	log.Println("Done...")
 	if err != nil {
 		app.errorJSON(w, err)
 		return
-	} else if !authResp.Authed {
-		payload := jsonResponse{
-			Error:   true,
-			Message: "failed to authenticate " + authPayload.Username,
-		}
-		app.writeJSON(w, http.StatusUnauthorized, payload)
-		return
 	}
+	//  else if !authResp.Authed {
+	// 	payload := jsonResponse{
+	// 		Error:   true,
+	// 		Message: "failed to authenticate " + authPayload.Username,
+	// 	}
+	// 	app.writeJSON(w, http.StatusUnauthorized, payload)
+	// 	return
+	// }
 
 	payload := jsonResponse{
 		Error:   false,
-		Message: "authenticated " + authPayload.Username,
+		Message: "created user " + cruResp.Username,
 	}
-
 	app.writeJSON(w, http.StatusAccepted, payload)
+}
 
+// a function that utilizes a GRPC conneciton to the auth
+// service to validify an email and a password
+func (app *Config) authenticate(w http.ResponseWriter, authPayload AuthPayload) {
+	conn, err := grpc.Dial("localhost:50001", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer conn.Close()
+
+	a := auth.NewAuthServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	authResp, err := a.AuthUser(ctx, &auth.AuthRequest{
+		ArgUser: &auth.User{
+			Password: authPayload.Password,
+
+			Username: &authPayload.Username,
+		},
+	})
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	//  else if !authResp.Authed {
+	// 	payload := jsonResponse{
+	// 		Error:   true,
+	// 		Message: "failed to authenticate " + authPayload.Username,
+	// 	}
+	// 	app.writeJSON(w, http.StatusUnauthorized, payload)
+	// 	return
+	// }
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "authenticated user " + authResp.Username,
+		Data:    authResp.PasetoToken,
+	}
+	log.Println("DOWN HERE")
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
